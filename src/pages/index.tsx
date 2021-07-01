d510c4c631916be99d638a9c1eb2a725eb5e865b
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-07-01 12:53:17 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Index = () => (
+   <div>
+      <h1>Hello world!</h1>
+      <p>You are using Matthieu LOCUSSOL&apos;s starter using Typescript and Next.js!</p>
+   </div>
+);
+
+export default Index;
