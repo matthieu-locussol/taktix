e778f8b071c1fac921fe4ead20869d973d964183
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-07-03 13:02:38 +0200

    feat: prisma client is now exposed correctly to fit with Next.js

--- src/utils/api/prisma.ts
+++ src/utils/api/prisma.ts
@@ -1,3 +1,15 @@
+// See: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+interface CustomNodeJSGlobal extends NodeJS.Global {
+   prisma: PrismaClient;
+}
+
+declare const global: CustomNodeJSGlobal;
+
+if (process.env.NODE_ENV === 'development') {
+   global.prisma = new PrismaClient();
+}
+
+export const prisma = global.prisma || new PrismaClient();
