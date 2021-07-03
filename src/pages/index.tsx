327c9d16b665176fd2505c60028ead949ff45ffb
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-07-03 13:03:43 +0200

    feat: removed /users/create endpoint & updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,35 @@
-import { api } from '@/utils/api/api';
 import type { User } from '@prisma/client';
-import React, { useState } from 'react';
+import { signIn, useSession, signOut } from 'next-auth/client';
+import React from 'react';
 import useSWR from 'swr';
-import { signIn } from 'next-auth/client';
 
 const Index = (): JSX.Element => {
+   const [session, loading] = useSession();
    const { data, error } = useSWR<User[]>('/api/users/list');
-   const [email, setEmail] = useState('');
-   const [createError, setCreateError] = useState(false);
-   const [createSuccess, setCreateSuccess] = useState(false);
-
-   const create = async () => {
-      try {
-         setCreateError(false);
-         setCreateSuccess(false);
-
-         await api.post<User>('/users/create', { email });
-         setCreateSuccess(true);
-      } catch (e) {
-         setCreateError(true);
-      }
-   };
 
    if (!data) return <p>Loading...</p>;
    if (error) return <p>Error: {error}!</p>;
+
+   const LoginButton = () =>
+      session?.user ? (
+         <>
+            <p>Welcome {session.user.name}</p>
+            <button type="button" onClick={() => signOut()}>
+               logout
+            </button>
+         </>
+      ) : (
+         <button type="button" onClick={() => signIn()}>
+            login
+         </button>
+      );
 
    return (
       <div>
          <h1>Hello world!</h1>
          <p>You are using Matthieu LOCUSSOL&apos;s starter using Typescript and Next.js!</p>
          <p>{JSON.stringify(data)}</p>
-         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-         <button type="button" onClick={create}>
-            Create user
-         </button>
-         {createError && <p>An error occurred while creating your account!</p>}
-         {createSuccess && <p>Successfully created your account!</p>}
-         <button type="button" onClick={() => signIn()}>
-            login
-         </button>
+         {loading ? <p>Loading...</p> : <LoginButton />}
       </div>
    );
 };
