90d0f3787feff1e6ba9641d4c7a9a492215e60a6
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-07-08 22:52:38 +0200

    tests: added tests for clamp utility function

---
+++ src/utils/__tests__/clamp.spec.ts
@@ -0,0 +1,18 @@
+import { clamp } from '../clamp';
+
+describe('clamp', () => {
+   test('value inside the range', () => {
+      const clamped = clamp(37, 20, 100);
+      expect(clamped).toBe(37);
+   });
+
+   test('value lower than the minimum', () => {
+      const clamped = clamp(17, 20, 100);
+      expect(clamped).toBe(20);
+   });
+
+   test('value greater than the maximum', () => {
+      const clamped = clamp(170, 20, 100);
+      expect(clamped).toBe(100);
+   });
+});
